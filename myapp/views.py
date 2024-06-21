7cf08baf4189be959cedbf0ec851e9c143781187
akichun97 <akichun97@gmail.com>
2024-06-21 14:46:35 +0600

    url_check fixing

--- myapp/views.py
+++ myapp/views.py
@@ -10,43 +10,39 @@ def index(request):
 
 def url_check(request):
     if request.method == 'POST':
-        url_check = request.POST.get('url')   
-           
+        url_check = request.POST.get('url')
         try:
-            response = requests.get(url_check.url, timeout=5)
-            
+            response = requests.get(url_check, timeout=200)
             if response.status_code == 200:
                 new_url = URL.objects.create(url=url_check, is_available=True)
-                return redirect('index.html')
+                return redirect('/')
             else:
                 new_url = URL.objects.create(url=url_check, is_available=False)
-                return redirect('index.html')
-                           
+                return redirect('/')
         except requests.exceptions.RequestException:
             new_url = URL.objects.create(url=url_check, is_available=False)
-            return redirect('index.html')
-        
-        return render(request, 'url_check.html')
+            return redirect('/')
+    return render(request, '/', {'urls': URL.objects.all()})
 
 
-# def all_urls_check(request):
-#     urls = URL.objects.all()
-#     for url in urls:
-#         try:
-#             response = request.get(url.url, timeout=5)
-#             if response.ststus_code == 200:
-#                 url.is_available = True
-#                 url.save()
-#         except requests.exceptions.RequestException:
-#             url.is_available = False
-#             url.save()
-#     return redirect('index.html')                
+def all_urls_check(request):
+    urls = URL.objects.all()
+    for url in urls:
+        try:
+            response = request.get(url.url, timeout=5)
+            if response.ststus_code == 200:
+                url.is_available = True
+                url.save()
+        except requests.exceptions.RequestException:
+            url.is_available = False
+            url.save()
+    return redirect('index.html')                
 
 
-def url_add(request):
-    if request.method == 'POST':
-        url = request.POST.get('url')
-        new_url = URL.objects.create(url=url)
-        return redirect('index')
+# def url_add(request):
+#     if request.method == 'POST':
+#         url = request.POST.get('url')
+#         new_url = URL.objects.create(url=url)
+#         return redirect('index')
     
-    return render(request, 'url_add.html')
+#     return render(request, 'index')
